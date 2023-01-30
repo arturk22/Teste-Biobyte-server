0e376a557cc05b56fa4faf99f322100a090f2113
arturk22 <alencar.artur00@gmail.com>
2023-01-30 16:52:43 -0300

    ajustando rota post

--- routes.js
+++ routes.js
@@ -2,11 +2,25 @@ const express = require('express');
 const router = express.Router();
 const connection = require('./configDb');
 
+// Verificação de emails já cadastrados
+router.get('/verificaEmail/:email', (req, res) => {
+  const email = req.params.email;
+  const emailQuery = `SELECT email FROM usersbiobyte WHERE email = '${email}'`;
+  connection.query(emailQuery, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else if (results.length > 0) {
+      return res.status(400).send({ message: 'Email já existe' });
+    } else {
+      return res.status(200).send({ message: 'Email disponível' });
+    }
+  });
+});
 
 // Criação de novos usuários
-router.post('conta/cadastro', (req, res) => {
+router.post('/conta/cadastro', (req, res) => {
   const { name, surname, email, password } = req.body;
-  // Verifica se o email já existe no banco de dados
+  // Verifica se o email já existe no banco de dados antes de adicionar um novo usuário
   const emailQuery = `SELECT email FROM usersbiobyte WHERE email = '${email}'`;
   connection.query(emailQuery, (err, results) => {
     if (err) {
@@ -26,22 +40,5 @@ router.post('conta/cadastro', (req, res) => {
     }
   });
 });
-
-
-// Verificação de emails já cadastrados
-router.get('/verificaEmail/:email', (req, res) => {
-  const email = req.params.email;
-  const emailQuery = `SELECT email FROM usersbiobyte WHERE email = '${email}'`;
-  connection.query(emailQuery, (err, results) => {
-    if (err) {
-      return res.send(err);
-    } else if (results.length > 0) {
-      return res.status(400).send({ message: 'Email já existe' });
-    } else {
-      return res.status(200).send({ message: 'Email disponível' });
-    }
-  });
-});
-
 
 module.exports = router;

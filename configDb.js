2778d858dc2ce2223a507566fb9086a347ceb942
arturk22 <alencar.artur00@gmail.com>
2023-01-30 19:57:05 -0300

    arquivo configdb para bd heroku

--- configDb.js
+++ configDb.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '12345678',
-    database: 'bioByte' // Nome do banco de dados
+host: 'us-cdbr-east-06.cleardb.net',
+user: 'bf34b01d18e870',
+password: '745a8de6',
+database: 'heroku_9df52e1963f49ba',
+multipleStatements: true,
+reconnect: true
 });
 
 module.exports = connection;

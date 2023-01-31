25de06458797d55ee2774fd6d84709b2d74a4bb3
arturk22 <alencar.artur00@gmail.com>
2023-01-30 23:15:26 -0300

    mudando para Draxlr

--- configDb.js
+++ configDb.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql2');
 
-// const connection = mysql.createConnection({
-//     host: 'us-cdbr-east-06.cleardb.net',
-//     user: 'b7efc2ce716366',
-//     password: 'b970fd7c',
-//     database: 'heroku_90dc31fc2144d80',
-//     port: 3306,
-// });
-
-
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'vkh7buea61avxg07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
-    user: 'root',
-    password: '12345678',
-    database: 'bioByte' // Nome do banco de dados
+    user: 'ulsrtt6ly052salg',
+    password: 'nc3gmhh0x2gweobo',
+    database: 'gjv6f51hi9oy7npy' // Nome do banco de dados
 });
+
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: '12345678',
+//     database: 'bioByte' // Nome do banco de dados
+// });
 
 connection.connect((err) => {
     if (err) throw err;
